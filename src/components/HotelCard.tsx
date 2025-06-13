9646e8177229177fd588af0725383fedd5f1df56
tanvir2108033 <tanvir.r.me@gmail.com>
2025-06-13 15:03:27 +0600

    first commit

--- src/components/HotelCard.tsx
+++ src/components/HotelCard.tsx
@@ -11,18 +11,20 @@ interface HotelCardProps {
 
 const HotelCard: React.FC<HotelCardProps> = ({ id, title, description, image }) => {
   return (
-    <div className="col-md-4 card">
+    <div className="col-12 col-sm-6 col-md-4 mb-4">
       <Link to={`/hotel/${id}`} className="text-decoration-none text-dark">
-        <Card className="h-100 shadow-sm">
-          <Card.Img
-            variant="top"
-            src={image}
-            className="card-img-top"
-            alt={title}
-          />
+        <Card className="h-100 shadow-sm hover-card">
+          <div className="card-img-container">
+            <Card.Img
+              variant="top"
+              src={image}
+              className="card-img-top"
+              alt={title}
+            />
+          </div>
           <Card.Body>
-            <Card.Title>{title}</Card.Title>
-            <Card.Text>{description}</Card.Text>
+            <Card.Title className="h5 mb-3">{title}</Card.Title>
+            <Card.Text className="text-muted">{description}</Card.Text>
           </Card.Body>
         </Card>
       </Link>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,20 +8,23 @@ const Navbar: React.FC = () => {
     <BootstrapNavbar expand="lg" className="navbar-dark" style={{ backgroundColor: '#004d4d' }}>
       <Container>
         <BootstrapNavbar.Brand as={Link} to="/" style={{ fontFamily: 'Poppins, sans-serif' }}>
-          <img src={logo} alt="SmolWorld Logo" height="50" /> SmolWorld
+          <img src={logo} alt="SmolWorld Logo" height="50" className="d-inline-block align-top" /> 
+          <span className="d-none d-sm-inline"> SmolWorld</span>
         </BootstrapNavbar.Brand>
-        <BootstrapNavbar.Toggle aria-controls="navbarNav" />
+        <BootstrapNavbar.Toggle aria-controls="navbarNav" className="border-0" />
         <BootstrapNavbar.Collapse id="navbarNav">
-          <Nav className="ms-auto d-flex align-items-center gap-2">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/hotels">Hotels</Nav.Link>
-            <Nav.Link as={Link} to="/tickets">Tickets</Nav.Link>
-            <Nav.Link as={Link} to="/location">Destination</Nav.Link>
-            <Nav.Link as={Link} to="/events">Events</Nav.Link>
-            <Nav.Link as={Link} to="/cost">Cost</Nav.Link>
-            <Nav.Link as={Link} to="/packages">Packages</Nav.Link>
-            <Link to="/register" className="btn btn-light ms-2">Sign Up</Link>
-            <Link to="/login" className="btn btn-outline-light ms-2">Log In</Link>
+          <Nav className="ms-auto d-flex align-items-center gap-2 flex-wrap">
+            <Nav.Link as={Link} to="/" className="text-center">Home</Nav.Link>
+            <Nav.Link as={Link} to="/hotels" className="text-center">Hotels</Nav.Link>
+            <Nav.Link as={Link} to="/tickets" className="text-center">Tickets</Nav.Link>
+            <Nav.Link as={Link} to="/location" className="text-center">Destination</Nav.Link>
+            <Nav.Link as={Link} to="/events" className="text-center">Events</Nav.Link>
+            <Nav.Link as={Link} to="/cost" className="text-center">Cost</Nav.Link>
+            <Nav.Link as={Link} to="/packages" className="text-center">Packages</Nav.Link>
+            <div className="d-flex flex-wrap justify-content-center gap-2 mt-2 mt-lg-0">
+              <Link to="/register" className="btn btn-light">Sign Up</Link>
+              <Link to="/login" className="btn btn-outline-light">Log In</Link>
+            </div>
           </Nav>
         </BootstrapNavbar.Collapse>
       </Container>
